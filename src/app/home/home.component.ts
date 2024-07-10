dc0aab21a63d71e0bf967ad953d67e8a08ff92b9
Dan Vitoriano <vitoriano08@gmail.com>
2024-07-10 01:30:05 -0300

    add components and interfaces

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-  `,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
