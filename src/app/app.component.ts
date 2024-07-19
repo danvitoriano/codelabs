eca4f474357fae1021da4054c4fdc8121239bf93
Dan Vitoriano <vitoriano08@gmail.com>
2024-07-18 23:31:48 -0300

    add basic route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
